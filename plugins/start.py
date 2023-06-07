0534bbe9bdb74aa404eef2100a11e08839f40535
Rascal72 <123348045+Rascal72@users.noreply.github.com>
2023-06-07 12:01:03 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -77,6 +77,7 @@ async def start_command(client: Client, message: Message):
 
             try:
                 l = await msg.copy(chat_id=message.from_user.id, caption = caption, parse_mode = ParseMode.HTML, reply_markup = reply_markup, protect_content=PROTECT_CONTENT)
+                await asyncio.sleep(0.5)
                 await asyncio.sleep(30)
                 await l.delete()
             except FloodWait as e:
